d129f9a869b9eda11aefa8dd3dba5b4aa2261e3a
eurosandcents <2kul4yhu@gmail.com>
2026-03-12 19:29:48 +0000

    v0.1.0 | Created a basic flask project. Check changes.log

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# CS1116 CA1
+# CS1116 CA1!!!
+
+# Dependencies:
+1. flask
+2. flask-wtf
+3. flask-session
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_session import Session
+from flask import session, g
+
+import database
+
+#region CONFIG
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.teardown_appcontext(database.close_db)
+Session(app)
+#endregion
+
+# for all routes:
+# - pass title arg for base template
+
+@app.route("/", methods=["GET","POST"])
+def home() -> str:
+    return render_template(
+        "generic/home.html", 
+        title="Home"
+    )
